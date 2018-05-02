dbb41d8983aa96df14de25aa1820a2d9f1b3a2fa
Jason Wilson <jason@wilsons.io>
2018-05-02 17:25:04 -0400

    Filter functionality

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -11,7 +11,11 @@ import { GenderPipe } from "../shared/pipes/gender.pipe";
 import { GalacticYearPipe } from "../shared/pipes/galactic-year.pipe";
 import { Subject } from "rxjs/Subject";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { from } from "rxjs/observable/from";
 import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/withLatestFrom";
+import "rxjs/add/operator/skipWhile";
+import "rxjs/add/operator/zip";
 
 @Component({
 	selector: "app-list",
@@ -29,6 +33,7 @@ export class ListComponent implements OnInit {
 	massOptionsSubject = new BehaviorSubject<FilterAttributeOption[]>([]);
 	hairColorOptionsSubject = new BehaviorSubject<FilterAttributeOption[]>([]);
 	skinColorOptionsSubject = new BehaviorSubject<FilterAttributeOption[]>([]);
+	eyeColorOptionsSubject = new BehaviorSubject<FilterAttributeOption[]>([]);
 	genderOptionsSubject = new BehaviorSubject<FilterAttributeOption[]>([]);
 
 	constructor(private peopleService: PeopleService) {}
@@ -67,6 +72,9 @@ export class ListComponent implements OnInit {
 			["skin_color"].subscribe(this.skinColorOptionsSubject);
 		this.peopleService
 			.getFilterOptions()
+			["eye_color"].subscribe(this.eyeColorOptionsSubject);
+		this.peopleService
+			.getFilterOptions()
 			["gender"].subscribe(this.genderOptionsSubject);
 
 		// Subscribe to the querySubject to the searchPeople observable
@@ -77,7 +85,7 @@ export class ListComponent implements OnInit {
 
 	sortOptions(subject) {
 		return subject.getValue().sort((a, b) => {
-			console.log(`Comparing ${a.value} and ${b.value}...`);
+			//console.log(`Comparing ${a.value} and ${b.value}...`);
 			return a.value.localeCompare(b.value, undefined, {
 				numeric: true,
 				sensitivity: "base"
@@ -88,11 +96,95 @@ export class ListComponent implements OnInit {
 		console.info(`Search query received: '${queryString}'`);
 		this.peopleService.searchPeople(queryString).subscribe(
 			people => {
-				console.log("Searched data received from Subject...");
-				console.table(people);
-				this.people = people;
+				//console.log("Searched data received from Subject...");
+				//console.table(people);
+				this.people = this.filterPeople(people);
 			},
 			error => console.error(error)
 		);
+	}
+
+	filterSource(filterSubject: BehaviorSubject<FilterAttributeOption[]>) {
+		return filterSubject
+			.map(options => {
+				return options.filter(option => {
+					//console.log(option.isActiveFilter.getValue());
+					return option.isActiveFilter.getValue();
+				});
+			})
+			.map(options => options.map(option => option.value));
+	}
+	filterPeople(people: Organism[]): Organism[] {
+		console.log("Filtering...");
+		let filteredPeople = [];
+		const peopleSource = from(people);
+		const heightSource = this.filterSource(this.heightOptionsSubject);
+		const massSource = this.filterSource(this.massOptionsSubject);
+		const hairColorSource = this.filterSource(this.hairColorOptionsSubject);
+		const eyeColorSource = this.filterSource(this.eyeColorOptionsSubject);
+		const skinColorSource = this.filterSource(this.skinColorOptionsSubject);
+		const genderSource = this.filterSource(this.genderOptionsSubject);
+
+		const filterSource = heightSource.zip(
+			massSource,
+			hairColorSource,
+			eyeColorSource,
+			skinColorSource,
+			genderSource,
+			(f1, f2, f3, f4, f5, f6) => {
+				return {
+					height: f1,
+					mass: f2,
+					hair_color: f3,
+					eye_color: f4,
+					skin_color: f5,
+					gender: f6
+				};
+			}
+		);
+
+		console.log("People source: ", peopleSource);
+
+		peopleSource
+			.withLatestFrom(filterSource, (person, options) => {
+				return { person: person, optionsArray: options };
+			})
+			.filter(check => {
+				return this.filterCheck(check, "height");
+			})
+			.filter(check => {
+				return this.filterCheck(check, "mass");
+			})
+			.filter(check => {
+				return this.filterCheck(check, "hair_color");
+			})
+			.filter(check => {
+				return this.filterCheck(check, "eye_color");
+			})
+			.filter(check => {
+				return this.filterCheck(check, "skin_color");
+			})
+			.filter(check => {
+				return this.filterCheck(check, "gender");
+			})
+			.map(checked => checked.person)
+			.reduce(
+				(acc: ReadonlyArray<Organism>, person) => acc.concat(person),
+				[]
+			)
+			.subscribe(
+				people => (filteredPeople = people),
+				error => console.error(error)
+			);
+		return filteredPeople;
+	}
+
+	filterCheck(check, attribute) {
+		if (
+			check.optionsArray[attribute].length === 0 ||
+			check.optionsArray[attribute].includes(check.person[attribute])
+		) {
+			return true;
+		}
 	}
 }

1eeb53a7b3b7ea486ed355af8524e0d0b801bebd
Pavel Svitek <svitek.pavel@gmail.com>
2018-12-27 08:24:59 +0100

    add tests for numbers for organism[Height,Weight]

--- src/app/shared/pipes/organismHeight.spec.ts
+++ src/app/shared/pipes/organismHeight.spec.ts
@@ -1,31 +1,33 @@
 import { OrganismHeightPipe } from './organismHeight';
 
 describe('OrganismHeightPipe', () => {
-    let pipe: OrganismHeightPipe;
+	let pipe: OrganismHeightPipe;
 
-    beforeEach(() => {
-        pipe = new OrganismHeightPipe();
-    });
+	beforeEach(() => {
+		pipe = new OrganismHeightPipe();
+	});
 
-    it('should create an instance of OrganismHeightPipe', () => {
-        expect(pipe).toBeTruthy();
-    });
+	it('should create an instance of OrganismHeightPipe', () => {
+		expect(pipe).toBeTruthy();
+	});
 
-    it('should return "172 cm" for "172" input', () => {
-        expect(pipe.transform('172')).toBe('172 cm');
-    });
+	it('should return "172 cm" for "172" input', () => {
+		expect(pipe.transform('172')).toBe('172 cm');
+		expect(pipe.transform(172)).toBe('172 cm');
+	});
 
-    it('should return "20 cm" for "20" input', () => {
-        expect(pipe.transform('20')).toBe('20 cm');
-    });
+	it('should return "20 cm" for "20" input', () => {
+		expect(pipe.transform('20')).toBe('20 cm');
+		expect(pipe.transform(20)).toBe('20 cm');
+	});
 
-    it('should return "" for "" input', () => {
-        expect(pipe.transform('')).toBe('');
-    });
+	it('should return "" for "" input', () => {
+		expect(pipe.transform('')).toBe('');
+	});
 
-    it('should not add "cm" for non-a-number input', () => {
-        expect(pipe.transform('')).toBe('');
-        expect(pipe.transform('twenty')).toBe('twenty');
-        expect(pipe.transform('random')).toBe('random');
-    });
-})
+	it('should not add "cm" for non-a-number input', () => {
+		expect(pipe.transform('')).toBe('');
+		expect(pipe.transform('twenty')).toBe('twenty');
+		expect(pipe.transform('random')).toBe('random');
+	});
+});
--- src/app/shared/pipes/organismWeight.spec.ts
+++ src/app/shared/pipes/organismWeight.spec.ts
@@ -1,31 +1,33 @@
 import { OrganismWeightPipe } from './organismWeight';
 
 describe('OrganismWeightPipe', () => {
-    let pipe: OrganismWeightPipe;
+	let pipe: OrganismWeightPipe;
 
-    beforeEach(() => {
-        pipe = new OrganismWeightPipe();
-    });
+	beforeEach(() => {
+		pipe = new OrganismWeightPipe();
+	});
 
-    it('should create an instance of OrganismWeightPipe', () => {
-        expect(pipe).toBeTruthy();
-    });
+	it('should create an instance of OrganismWeightPipe', () => {
+		expect(pipe).toBeTruthy();
+	});
 
-    it('should return "172 kg" for "172" input', () => {
-        expect(pipe.transform('172')).toBe('172 kg');
-    });
+	it('should return "172 kg" for "172" input', () => {
+		expect(pipe.transform('172')).toBe('172 kg');
+		expect(pipe.transform(172)).toBe('172 kg');
+	});
 
-    it('should return "20 kg" for "20" input', () => {
-        expect(pipe.transform('20')).toBe('20 kg');
-    });
+	it('should return "20 kg" for "20" input', () => {
+		expect(pipe.transform('20')).toBe('20 kg');
+		expect(pipe.transform(20)).toBe('20 kg');
+	});
 
-    it('should return "" for "" input', () => {
-        expect(pipe.transform('')).toBe('');
-    });
+	it('should return "" for "" input', () => {
+		expect(pipe.transform('')).toBe('');
+	});
 
-    it('should not add "kg" for non-a-number input', () => {
-        expect(pipe.transform('')).toBe('');
-        expect(pipe.transform('twenty')).toBe('twenty');
-        expect(pipe.transform('random')).toBe('random');
-    });
-})
+	it('should not add "kg" for non-a-number input', () => {
+		expect(pipe.transform('')).toBe('');
+		expect(pipe.transform('twenty')).toBe('twenty');
+		expect(pipe.transform('random')).toBe('random');
+	});
+});
